65a01c0d018aea27e62f5b3a3c3078253229e214
Ethan Zimbelman <ethan.zimbelman@slack-corp.com>
2023-04-28 13:26:18 -0700

    add stronger typing for the trigger

--- triggers/give_kudos.ts
+++ triggers/give_kudos.ts
@@ -1,10 +1,11 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
+import { GiveKudosWorkflow } from "../workflows/give_kudos.ts";
 
-const trigger: Trigger = {
+const trigger: Trigger<typeof GiveKudosWorkflow.definition> = {
   type: "shortcut",
   name: "Give some kudos",
   description: "Broadcast your appreciation with kind words and a GIF",
-  workflow: "#/workflows/give_kudos_workflow",
+  workflow: `#/workflows/${GiveKudosWorkflow.definition.callback_id}`,
   inputs: {
     interactivity: {
       value: "{{data.interactivity}}",
