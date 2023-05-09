f4b9dff64f47a7a42aae8b306193b848d1e80393
Filip Maj <fmaj@slack-corp.com>
2023-05-09 13:18:32 -0400

    Use trigger data and type enumerations in trigger definition.

--- triggers/give_kudos.ts
+++ triggers/give_kudos.ts
@@ -1,4 +1,5 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 import { GiveKudosWorkflow } from "../workflows/give_kudos.ts";
 
 /**
@@ -7,13 +8,13 @@ import { GiveKudosWorkflow } from "../workflows/give_kudos.ts";
  * Learn more: https://api.slack.com/automation/triggers/link
  */
 const trigger: Trigger<typeof GiveKudosWorkflow.definition> = {
-  type: "shortcut",
+  type: TriggerTypes.Shortcut,
   name: "Give some kudos",
   description: "Broadcast your appreciation with kind words and a GIF",
   workflow: `#/workflows/${GiveKudosWorkflow.definition.callback_id}`,
   inputs: {
     interactivity: {
-      value: "{{data.interactivity}}",
+      value: TriggerContextData.Shortcut.interactivity,
     },
   },
 };
