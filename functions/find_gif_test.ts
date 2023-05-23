1acc3a3f8df6e29178f70ec165c7c7f77d373c75
Cal Bullard <cbullard@slack-corp.com>
2023-05-23 11:49:48 +1000

    Added new tests for each of the selecteable options in the kudo_vibe

--- functions/find_gif_test.ts
+++ functions/find_gif_test.ts
@@ -20,6 +20,71 @@ Deno.test("Find a GIF with the appreciation tag", async () => {
   assertArrayIncludes(expectedGIFs, [outputs?.URL]);
 });
 
+Deno.test("Find a GIF with the celebration tag", async () => {
+  const inputs = { vibe: "Celebrating a victory 🏆" };
+  const expectedGIFs = [
+    "https://media3.giphy.com/media/DhstvI3zZ598Nb1rFf/giphy-downsized.gif",
+    "https://media.giphy.com/media/WKdPOVCG5LPaM/giphy.gif",
+    "https://media0.giphy.com/media/kyLYXonQYYfwYDIeZl/giphy.gif",
+    "https://media2.giphy.com/media/o75ajIFH0QnQC3nCeD/giphy.gif",
+    "https://media3.giphy.com/media/xTiN0CNHgoRf1Ha7CM/giphy-downsized.gif",
+  ];
+
+  const { outputs } = await FindGIF(createContext({ inputs }));
+  assertArrayIncludes(expectedGIFs, [outputs?.URL]);
+});
+
+Deno.test("Find a GIF with the thankful tag", async () => {
+  const inputs = { vibe: "Thankful for great teamwork ⚽️" };
+  const expectedGIFs = [
+    "https://media2.giphy.com/media/3oEjHWXddcCOGZNmFO/giphy.gif",
+    "https://media2.giphy.com/media/ZfK4cXKJTTay1Ava29/giphy.gif",
+    "https://media1.giphy.com/media/NEvPzZ8bd1V4Y/giphy-downsized.gif",
+  ];
+
+  const { outputs } = await FindGIF(createContext({ inputs }));
+  assertArrayIncludes(expectedGIFs, [outputs?.URL]);
+});
+
+Deno.test("Find a GIF with the amazed tag", async () => {
+  const inputs = { vibe: "Amazed at awesome work ☄️" };
+  const expectedGIFs = [
+    "https://media.giphy.com/media/3o6Zt2jsgmiAUl2ydi/giphy.gif",
+    "https://media1.giphy.com/media/Lcn0yF1RcLANG/giphy-downsized.gif",
+    "https://media0.giphy.com/media/rgIdiNjWC933y/giphy.gif",
+    "https://media3.giphy.com/media/WFyvwq3xoTTMs/giphy.gif",
+    "https://media3.giphy.com/media/jz2VNqCrlFsPe/giphy.gif",
+  ];
+
+  const { outputs } = await FindGIF(createContext({ inputs }));
+  assertArrayIncludes(expectedGIFs, [outputs?.URL]);
+});
+
+Deno.test("Find a GIF with the excited tag", async () => {
+  const inputs = { vibe: "Excited for the future 🎉" };
+  const expectedGIFs = [
+    "https://media.giphy.com/media/WKdPOVCG5LPaM/giphy.gif",
+    "https://media0.giphy.com/media/rgIdiNjWC933y/giphy.gif",
+    "https://media2.giphy.com/media/o75ajIFH0QnQC3nCeD/giphy.gif",
+    "https://media3.giphy.com/media/xTiN0CNHgoRf1Ha7CM/giphy-downsized.gif",
+  ];
+
+  const { outputs } = await FindGIF(createContext({ inputs }));
+  assertArrayIncludes(expectedGIFs, [outputs?.URL]);
+});
+
+Deno.test("Find a GIF with the plants tag", async () => {
+  const inputs = { vibe: "No vibes, just plants 🪴" };
+  const expectedGIFs = [
+    "https://media.giphy.com/media/l3JDHv2xpLbQ7Tjdm/giphy.gif",
+    "https://media3.giphy.com/media/IbsQ8L4PTQ53SH7FnL/giphy.gif",
+    "https://media1.giphy.com/media/xUA7aOIFDR4ZgqLy8w/giphy.gif",
+  ];
+
+  const { outputs } = await FindGIF(createContext({ inputs }));
+  assertArrayIncludes(expectedGIFs, [outputs?.URL]);
+});
+
 Deno.test("Ensure otter gifs are returned if no vibe is selected", async () => {
   const inputs = { vibe: "" };
   const expectedGIFs = [
