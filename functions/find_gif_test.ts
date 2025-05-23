f55c082903c2ab13b9c42b39c97c6c5baf4325b6
William Bergamin <wbergamin@slack-corp.com>
2025-05-23 14:11:49 -0400

    use jsr imports

--- functions/find_gif_test.ts
+++ functions/find_gif_test.ts
@@ -1,5 +1,5 @@
 import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
-import { assertArrayIncludes } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+import { assertArrayIncludes } from "@std/assert";
 import FindGIF from "./find_gif.ts";
 
 /**
